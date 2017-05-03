db53652783c4a1c78ad650919dc585b3044fd40b
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2017-05-03 16:02:20 +0100

    Bump version: 0.0.0 → 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 setup(name='mathprogbasepy',
-      version='0.0.0',
+      version='0.1.0',
       author='Bartolomeo Stellato',
       description='Low level interface for mathematical programming solvers.',
       url='http://github.com/bstellato/mathprogbasepy/',
